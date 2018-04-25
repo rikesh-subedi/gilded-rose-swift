85ca566228cbe4aefda1c773f64b3e73f53856f7
Subedi, Rikesh <rikesh.subedi@sap.com>
2018-04-25 16:19:52 +0530

    Second Level Refactor

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -7,50 +7,25 @@
 //
 
 public class GildedRose {
+    
+    
     public static func updateQuality(items: Array<Item>) -> [Item] {
         var items = items
         for i in 0 ..< items.count {
-            //let item = items[i]
             switch items[i].name {
             case "Sulfuras, Hand of Ragnaros" :
                 continue
             case "Aged Brie":
-                if(items[i].sellIn < 1){
-                    items[i] = Item.increaseQuality(item: items[i])
-                }
                 items[i] = Item.increaseQuality(item: items[i])
-                items[i].sellIn = items[i].sellIn - 1
                 break
             case "Backstage passes to a TAFKAL80ETC concert":
-                if(items[i].sellIn < 1){
-                    items[i].quality = 0
-                }
-                else if items[i].sellIn < 6 {
-                    items[i] = Item.increaseQuality(item: items[i], by: 3)
-                }
-                else if items[i].sellIn < 11  {
-                    items[i] = Item.increaseQuality(item: items[i], by: 2)
-                } else {
-                    items[i] = Item.increaseQuality(item: items[i])
-                }
-                
-                items[i].sellIn = items[i].sellIn - 1
-                break
+                items[i] = Item.specialIncrease(item: items[i])
             case "Conjured Mana Cake":
-                if(items[i].sellIn < 1){
-                    items[i] = Item.decreaseQuality(item: items[i], by: 2)
-                }
                 items[i] = Item.decreaseQuality(item: items[i], by: 2)
-                items[i].sellIn = items[i].sellIn - 1
-                break
             default:
-                if(items[i].sellIn < 1){
-                    items[i] = Item.decreaseQuality(item: items[i])
-                }
-                items[i] = Item.decreaseQuality(item: items[i])
-                items[i].sellIn = items[i].sellIn - 1
-                break
+                items[i] = Item.decreaseQuality(item: items[i], by: 1)
             }
+            items[i].sellIn = items[i].sellIn - 1
         }
         
         return items
